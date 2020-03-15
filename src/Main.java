31a73c7cd1d787f84e96a5989ddc140567505ebe
ThePurityOfChaos <derickdew9001@gmail.com>
2020-03-14 22:40:03 -0700

    Did it on the 2nd try :D
    Pseudocode op

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,40 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+    System.out.println(Arrays.toString(autoComplete("h")));
+        System.out.println(Arrays.toString(autoComplete("se")));
+        System.out.println(Arrays.toString(autoComplete("sea")));
+        System.out.println(Arrays.toString(autoComplete("ho")));
+        System.out.println(Arrays.toString(autoComplete("xyz")));
+    }
+
+    public static String[] autoComplete(String input) {
+        String[] setOfWords = {"hello", "high", "seattle", "seatac", "see", "hollow", "how"};
+        char[][] setOfLetters = new char[setOfWords.length][]; //step 1
+        for (int i = 0; i < setOfLetters.length; i++) {
+            setOfLetters[i] = setOfWords[i].toCharArray();
+        }
+        char[] inputLetters = input.toCharArray(); //complete step 1
+        int countOfRemaining = setOfWords.length;
+        for (int i = 0; i < setOfLetters.length; i++) { //step 2
+            for (int j = 0; j < inputLetters.length; j++) { //this is always going to be the size of the input, so it's not based on O(n). Weird.
+                if (inputLetters[j] != setOfLetters[i][j]) {
+                    setOfLetters[i] = null;
+                    countOfRemaining--;
+                    break;
+                }
+            }
+        }//complete step 2
+        String[] results = new String[countOfRemaining]; //step 3
+        int preventNull = 0;
+        for (int i = 0; i <setOfWords.length ; i++) {
+            if(setOfLetters[i]!=null){
+                results[preventNull]= String.valueOf(setOfLetters[i]);
+                preventNull++;
+            }
+        }//complete step 3
+        return results;
     }
 }
